aa2f27e836c29b628fed942134e6ca961c645b69
CoderButNoob <sonaraniket20@gmail.com>
2025-05-05 15:06:28 +0530

    javascript

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello")
+// console.log("hello")
+
+document.getElementById("employeeForm").addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const name = document.getElementById("name").valuex;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const department = document.getElementById("department").value;
+
+    alert(
+        "Employee Registered:\n\n"+
+        "Name: "+ name + "\n"+
+        "Email: " + email + "\n"+
+        "Phone: " + phone + "\n"+
+        "Department: " + department
+    );
+
+    document.getElementById("employeeForm").reset()
+})
+console.log("Form submitted!");
